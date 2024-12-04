ef39ebe94cb293c7d34c281b06b956139861be7c
Ckk312 <17256501+Ckk312@users.noreply.github.com>
2024-12-04 03:37:45 -0500

    swap sign in and landing

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -7,8 +7,8 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LoginSignup />} />
-        <Route path="/landing" element={<LandingPage />} />
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/auth" element={<LoginSignup />} />
       </Routes>
     </Router>
   );
