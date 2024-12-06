fcfc2111c87b86f828ed1a650600ee553a56820c
Ckk312 <17256501+Ckk312@users.noreply.github.com>
2024-12-06 02:18:55 -0500

    added components in teamlayout

--- Frontend/src/components/TeamLayout/TeamLayout.tsx
+++ Frontend/src/components/TeamLayout/TeamLayout.tsx
@@ -1,11 +1,46 @@
 import { useState } from "react";
 
+async function getRoster(title: string): Promise<any> {
+    const header = new Headers();
+    header.append('Content-type', 'application/json')
+
+    return await fetch('https://www.ckk312.xyz/api/searchdocuments', {
+        method: 'POST',
+        body: JSON.stringify({ collection: 'All Teams' ,query: title }),
+        headers: header
+    });
+}
+
 /***
  * Team Layout React Component
  * 
  */
 export default function TeamLayout(props: any) {
+    const [roster, setRoster] = useState<any[]>([]);
 
+    let path = window.location.pathname;
+    const game = path.split(',');
+
+    const allRosters = ['Knights', 'Knights Academy', 'Knights Rising', 'Knights Pink']
+    let rosterNum = 2;
+
+    if (game[-1] === 'Valorant') {
+        rosterNum = 4;
+    }
+
+    else if (game[-1] === 'Splatoon') {
+        rosterNum = 3;
+    }
+
+    else if (game[-1] === 'Smash') {
+        rosterNum = 1;
+    }
+
+    const rosters = allRosters.slice(rosterNum);
+
+    const handleLoad = async () => {
+        setRoster((await getRoster(game[-1])).result);
+    }
 
     return (
         <>
@@ -13,8 +48,15 @@ export default function TeamLayout(props: any) {
                 <div id="team-banner">
 
                 </div>
-                <div id="team-info-wrapper">
-                    
+                <div id="team-info-wrapper" onLoad={handleLoad}>
+                    {
+                        rosters.map((team: string, index: number) => {
+                            const newRoster = roster.filter((player: any) => {
+                                return player.TeamAffiliation = team;
+                            })
+                            return <Roster key={index} roster={roster} />
+                        })
+                    }
 
                 </div>
             </div>
@@ -27,19 +69,28 @@ function Roster(props: any) {
 
     return (
         <>
-            <div className="roster-container" onClick={() => { setIsOpen(true) }}>
-                { isOpen &&
-                    <>
+            <div className="roster-container" >
+                <div className="roster-container-clickable" onClick={() => { setIsOpen(true) }}>
 
-                    </>    
-                }
+                </div>
+                <div className="roster-display">
+                    { isOpen &&
+                        <>
+                            {
+                                props.roster.map((player: any, index: number) => {
+                                    return <Player key={index} player={player.name} />
+                                })
+                            }
+                        </>
+                    }
+                </div>
+                
             </div>
         </>
     );
 }
 
 function Player(props: any) {
-
     return (
         <>
             <div className="player-container">
@@ -47,7 +98,7 @@ function Player(props: any) {
 
                 </div>
                 <h3>
-                    {}
+                    {props.player}
                 </h3>
             </div>
         </>
