8805f7833427228e33ab52f9f6ecdd549d1ead8d
giselle <112223697+giselleismad@users.noreply.github.com>
2024-12-06 23:32:20 -0500

    edit profile
    edit profile button to change name and such

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -5,10 +5,19 @@ import LandingPage from './components/LandingPage/LandingPage';
 import ForgotPassword from './components/ForgotPassword/ForgotPassword';
 import TopBar from './components/TopBar/TopBar';
 import ResetPassword from './components/ResetPassword/ResetPassword'; // Import ResetPassword page
+<<<<<<< Updated upstream
+=======
+import Teams from './components/Teams/Teams';
+import TeamLayout from './components/TeamLayout/TeamLayout';
+import Error from './components/Error/Error';
+import EditProfile from './components/EditProfile/EditProfile'
+import { UserProvider } from './context/UserContext';
+>>>>>>> Stashed changes
 
 
 function App() {
   return (
+<<<<<<< Updated upstream
     <Router>
       <Routes>
         <Route path="/" element={<TopBar />} >
@@ -20,6 +29,27 @@ function App() {
         </Route>
       </Routes>
     </Router>
+=======
+    <UserProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<TopBar />} >
+            <Route index element={<LandingPage />} />
+            <Route path="login" element={<LoginSignup />} />
+            <Route path="forgot-password" element={<ForgotPassword />} />
+            <Route path="reset-password" element={<ResetPassword />} />
+            <Route path="teams" >
+              <Route index element={<Teams />} />
+              <Route path="*" element={<TeamLayout />} />
+            </Route>
+            <Route path="edit-profile" element={<EditProfile />} />
+            <Route path="*" element={<Error />} />
+            {/* Add ResetPassword route */}
+          </Route>
+        </Routes>
+      </Router>
+      </UserProvider>
+>>>>>>> Stashed changes
   );
 }
 
--- Frontend/src/components/TopBar/TopBar.tsx
+++ Frontend/src/components/TopBar/TopBar.tsx
@@ -3,8 +3,15 @@ import { Outlet } from 'react-router-dom';
 
 import './TopBar.css';
 
+<<<<<<< Updated upstream
 export default function TopBar() {
     const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false); // Track login state
+=======
+function TopBar() {
+  const { isLoggedIn, firstName, lastName, setIsLoggedIn } = useUser();
+  console.log('User Data: ', {isLoggedIn, firstName, lastName});
+  const navigate = useNavigate();
+>>>>>>> Stashed changes
 
     const handleSignOut = () => {
         if (isLoggedIn) {
@@ -14,6 +21,7 @@ export default function TopBar() {
         }
     };
 
+<<<<<<< Updated upstream
     return (
         <>
             <div id="top-bar">
@@ -29,6 +37,44 @@ export default function TopBar() {
                     {isLoggedIn ? 'Sign Out' : 'Sign In'}
                 </button>
             </div>
+=======
+  const handleEditProfile = () => {
+    navigate('/edit-profile');
+  };
+
+  return (
+    <>
+      <div id="top-bar">
+        <button id="home-btn" onClick={() => navigate('/')}>
+          Home
+        </button>
+        <h1 id="project-name">
+          {isLoggedIn
+            ? `Welcome ${firstName} ${lastName}`
+            : 'Welcome!'}
+        </h1>
+        {isLoggedIn ? (
+          <div>
+            <button id="prof-btn" onClick={handleEditProfile}>
+              Edit Profile
+            </button>
+            <button id="auth-btn" onClick={handleSignOut}>
+              Sign Out
+            </button>
+          </div>
+        ) : (
+          <button id="auth-btn" onClick={() => navigate('/login')}>
+            Admin<br /> Sign In
+          </button>
+        )}
+      </div>
+      <Outlet />
+    </>
+  );
+}
+
+export default React.memo(TopBar);
+>>>>>>> Stashed changes
 
             <Outlet />
         </>
