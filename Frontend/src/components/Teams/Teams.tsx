aa5930358a1543fe73824e6fdc55ac4de127e2f7
Ckk312 <17256501+Ckk312@users.noreply.github.com>
2024-12-08 17:35:06 -0500

    teamLayout captain updates

--- Frontend/src/components/Teams/Teams.tsx
+++ Frontend/src/components/Teams/Teams.tsx
@@ -1,10 +1,11 @@
+import { useNavigate } from 'react-router-dom';
 import './Teams.css'
 
 /***
  * 
  */
 export default function Teams() {
-    const teamInfo: [string, string][] = [['Apex Legends', 'https://i.ibb.co/FBCBb7m/Apex-Banner.webp'], ['Call of Duty', 'https://i.ibb.co/gr6xvCv/COD-Logo.webp'], ['League of Legends', 'https://i.ibb.co/xzpdD55/League-Banner.webp'], ['Overwatch 2', 'https://i.ibb.co/ZSm1RLf/Overwatch-Banner.webp'], ['Smash Ultimate', 'https://i.ibb.co/tK45Tgy/SSBU-Banner.webp'], ['Splatoon 3', 'https://i.ibb.co/R04p1bb/Splatoon3-Banner.webp'], ['Tom Clancy\'s: Rainbow Six Siege', 'https://i.ibb.co/KX3PjJV/R6-Logo.webp'], ['Valorant', 'https://i.ibb.co/YDnyPpY/Valorant-Banner.webp']];
+    const teamInfo: [string, string][] = [['Apex Legends', 'https://i.ibb.co/FBCBb7m/Apex-Banner.webp'], ['Call of Duty', 'https://i.ibb.co/gr6xvCv/COD-Logo.webp'], ['League of Legends', 'https://i.ibb.co/xzpdD55/League-Banner.webp'], ['Overwatch 2', 'https://i.ibb.co/ZSm1RLf/Overwatch-Banner.webp'], ['Smash Ultimate', 'https://i.ibb.co/tK45Tgy/SSBU-Banner.webp'], ['Splatoon 3', 'https://i.ibb.co/R04p1bb/Splatoon3-Banner.webp'], ['Tom Clancy\'s: Rainbow Six Siege', 'https://i.ibb.co/KX3PjJV/R6-Logo.webp'], ['Valorant', 'https://i.ibb.co/YDnyPpY/Valorant-Banner.webp'], ['Rocket League', ''], ['CounterStrike', '']];
 
     return (
         <>
@@ -26,6 +27,8 @@ export default function Teams() {
  * 
  */
 function TeamCard(props: any) {
+    const navigate = useNavigate();
+
     let name = props.name;
     if (props.name === 'Tom Clancy\'s: Rainbow Six Siege') {
         name = 'Rainbow Six Siege';
@@ -33,7 +36,7 @@ function TeamCard(props: any) {
 
     return (
         <>
-            <div className="team-cards" onClick={() => { window.location.href = '/teams/' + (name as string).replaceAll(' ', '') }}>
+            <div className="team-cards" onClick={() => { navigate('/teams/' + (name as string).replaceAll(' ', '')) }}>
                 <p>
                     {props.name}
                 </p>
