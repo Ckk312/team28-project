baba4133fabfe1685703165d56ebeb8c0060e112
Ckk312 <17256501+Ckk312@users.noreply.github.com>
2024-12-09 05:31:34 -0500

    oops

--- Frontend/src/components/TeamLayout/Helper.tsx
+++ Frontend/src/components/TeamLayout/Helper.tsx
@@ -159,32 +159,6 @@ export function getPastMatches(matches : Match[]) {
 
 // ----------------------------------------
 
-export function getPastMatches(matches : Match[]) {
-    // Get the current time in milliseconds
-    console.log("List of Possible Old Matches");
-    console.log(matches)
-
-    // Step 1: Map the matches to include the time difference from the current date
-    const pastMatches = matches
-        .map((match: Match) => {
-            const matchDate = new Date(match.Date * 1000); // Convert Unix timestamp
-            const timeDifference = matchDate.getTime() - Date.now();
-
-            return timeDifference <= 0
-                ? { ...match, date: matchDate, timeDifference }
-                : null; // Exclude past matches
-        })
-        .filter(Boolean) // Remove null values
-        .sort((a: any, b: any) => a.timeDifference - b.timeDifference); // Sort by time difference
-
-        console.log("Old Matches");
-        console.log(pastMatches);
-
-    return pastMatches;
-}
-
-// ----------------------------------------
-
 export function spaceUppercase(text: string) : string {
     if (text.length === 0 || !text) {
         return "";
